36f9fee456f713d8e4841449890da466e03f9222
Sanjana Keshari <147981954+Sanjanakeshari@users.noreply.github.com>
2026-06-03 12:15:55 +0530

    updated the logic

--- timer_algos.js
+++ timer_algos.js
@@ -3,7 +3,7 @@ function bubbleSort_t(arr) {
 
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - i - 1; j++) {
-            //comapre return arr[j]>arr[j+1]
+            //comapring return arr[j]>arr[j+1]
 
             if (compare(arr, j, j + 1)) {
                 swap(arr, j, j + 1);
